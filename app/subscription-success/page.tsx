3e93ea50264c47a250d718d94c0fef9a96111d6b
shalri <fsaonlinework777@gmail.com>
2024-06-03 21:29:15 +0800

    Adding Suspense from react

--- app/subscription-success/page.tsx
+++ app/subscription-success/page.tsx
@@ -1,9 +1,12 @@
 import ThankYou from "@/components/ThankYou";
+import { Suspense } from "react";
 
 export default function ThankYouPage() {
   return (
     <main>
-      <ThankYou />
+      <Suspense fallback={<div>loading..</div>}>
+        <ThankYou />
+      </Suspense>
     </main>
   );
 }
