52298d45c1c26d49cb4590ea7ec84535d1c12cee
shalri <fsaonlinework777@gmail.com>
2024-06-03 21:09:49 +0800

    test deployment

--- components/ThankYou.tsx
+++ components/ThankYou.tsx
@@ -1,32 +1,33 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 
 export default function ThankYou() {
-  // const searchParams = useSearchParams();
-  // const emailParam = searchParams.get("email");
-  // const email = emailParam ? decodeURIComponent(emailParam) : "unknown email";
+  const searchParams = useSearchParams();
+  const emailParam = searchParams.get("email");
+  const email = emailParam ? decodeURIComponent(emailParam) : "unknown email";
   // const router = useRouter();
   //
   // const handleDismiss = () => {
   //   router.push("/");
   // };
-  const searchParams = useSearchParams();
-  const emailParam = searchParams.get("email");
-  const [email, setEmail] = useState<string | null>(null);
-  const router = useRouter();
+  // const searchParams = useSearchParams();
+  // const emailParam = searchParams.get("email");
+  // const [email, setEmail] = useState<string | null>(null);
+  // const router = useRouter();
 
-  useEffect(() => {
-    // Ensure this code only runs on the client side
-    if (emailParam) {
-      setEmail(decodeURIComponent(emailParam));
-    }
-  }, [emailParam]);
+  // useEffect(() => {
+  //   // Ensure this code only runs on the client side
+  //   if (emailParam) {
+  //     setEmail(decodeURIComponent(emailParam));
+  //   }
+  // }, [emailParam]);
 
-  const handleDismiss = () => {
-    router.push("/");
-  };
+  // const handleDismiss = () => {
+  //   router.push("/");
+  // };
 
   return (
     <section>
@@ -39,7 +40,8 @@ export default function ThankYou() {
         A confirmatin email has been sent to {email}. Please open it and click
         the button insided to confirm your subscription.
       </p>
-      <button onClick={handleDismiss}>Dismiss message</button>
+      {/* <button onClick={handleDismiss}>Dismiss message</button> */}
+      <Link href="/">Dismiss message</Link>
     </section>
   );
 }
