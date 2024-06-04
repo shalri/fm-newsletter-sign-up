c6fa8aee4c2b8e642671f6ed2d5480f61a1689ea
shalri <fsaonlinework777@gmail.com>
2024-06-04 11:33:20 +0800

    Improve suspense loader layout

--- app/subscription-success/page.tsx
+++ app/subscription-success/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function ThankYouPage() {
   return (
-    <main>
+    <main className="min-h-dvh grid-cols-1 content-center">
       <Suspense fallback={<div className="loader" />}>
         <ThankYou />
       </Suspense>
